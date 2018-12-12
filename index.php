6ce93f4d1b9d4a54c525c1b113afdc36873c91bf
baselinerahul <34484037+baselinerahul@users.noreply.github.com>
2018-12-12 14:08:10 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,14 @@
 <?php
 
 include('functions.php');
-/*$data=get_users();
+$data=get_users();
  while($row = pg_fetch_row($data)) {
 		  echo "ID = ". $row[0] . "\n";
 		  echo "NAME = ". $row[1] ."\n";
 		  echo "ADDRESS = ". $row[2] ."\n";
 		  echo "SALARY =  ".$row[4] ."\n\n";
 	   }
-*/
-echo $d=database_connect();
+
 
 ?>
 
