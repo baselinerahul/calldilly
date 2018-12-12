6e5672b1bfe2124cd9641570a62c8e9000d501f3
baselinerahul <34484037+baselinerahul@users.noreply.github.com>
2018-12-12 13:53:57 +0530

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,45 @@
+<?php
+    
+	function database_connect() {
+		$db_connection = pg_connect("host=ec2-184-72-239-186.compute-1.amazonaws.com
+	 dbname=dfkcsq047ej0lu user=fzxqjsjlxieohl password=fbdfcaa97415b4062f460a6f3abda63d4bd1565766cdf382d536c4e66a67cf27");
+		
+	}
+	function user_registeration($data){
+		$db_connection=database_connect();
+	$insert = "INSERT INTO USERS (ID,USERNAME,EMAIL,PASSWORD,PHONE) VALUES ('".$data['id']."','".$data['username']."', '".$data['email']."', '".$data['password']."', '".$data['phone']."')";
+		# Execute query
+		if (pg_query($db_connection,$insert)) {
+			echo "Data entered successfully. ";
+		}
+		else {
+			echo "Data entry unsuccessful. ";
+			echo pg_last_error($db_connection);
+		}
+	
+		
+	}
+	function get_users(){
+		$db_connection=database_connect();
+	$select = "SELECT * FROM USERS";
+	 $ret = pg_query($db_connection, $select);
+	   if(!$ret) {
+		  echo pg_last_error($db_connection);
+		  exit;
+	   } 
+	  
+	   return $ret;
+	   
+
+	}
+	
+	
+	   
+	   
+	
+
+
+ 
+	
+?>
+
