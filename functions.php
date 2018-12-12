1d519f5bbcd4794b2d553143e6d35cd04d3c5403
baselinerahul <34484037+baselinerahul@users.noreply.github.com>
2018-12-12 14:05:33 +0530

    Update functions.php

--- functions.php
+++ functions.php
@@ -3,7 +3,11 @@
 	function database_connect() {
 		$db_connection = pg_connect("host=ec2-184-72-239-186.compute-1.amazonaws.com
 	 dbname=dfkcsq047ej0lu user=fzxqjsjlxieohl password=fbdfcaa97415b4062f460a6f3abda63d4bd1565766cdf382d536c4e66a67cf27");
-		
+		if($db_connection){
+			return 'connected';
+		}else{
+			return 'not';
+		}
 	}
 	function user_registeration($data){
 		$db_connection=database_connect();
