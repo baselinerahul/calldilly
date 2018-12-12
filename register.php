5f8313e81c08815be14469cc30dbd5bf32cdc2e8
baselinerahul <34484037+baselinerahul@users.noreply.github.com>
2018-12-12 14:15:19 +0530

    Update and rename regiter.php to register.php

---
+++ register.php
@@ -0,0 +1,14 @@
+<?php
+include('functions.php');
+
+$data=user_registeration($_POST);
+ while($row = pg_fetch_row($data)) {
+		  echo "ID = ". $row[0] . "\n";
+		  echo "NAME = ". $row[1] ."\n";
+		  echo "ADDRESS = ". $row[2] ."\n";
+		  echo "SALARY =  ".$row[4] ."\n\n";
+	   }
+?>
+
+
+?>
