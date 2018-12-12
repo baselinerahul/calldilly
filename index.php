f7eac4a0372307290ff7af32aef93e88189708dd
baselinerahul <34484037+baselinerahul@users.noreply.github.com>
2018-12-12 13:23:17 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     $db_connection = pg_connect("host=ec2-184-72-239-186.compute-1.amazonaws.com
  dbname=dfkcsq047ej0lu user=fzxqjsjlxieohl password=fbdfcaa97415b4062f460a6f3abda63d4bd1565766cdf382d536c4e66a67cf27");
      if($db_connection) {
-		$create="CREATE TABLE COMPANY(
+		/* $create="CREATE TABLE COMPANY(
 			   ID INT PRIMARY KEY     NOT NULL,
 			   NAME           TEXT    NOT NULL,
 			   AGE            INT     NOT NULL,
@@ -29,7 +29,23 @@
 			echo "Error creating table. ";
 		}
 	
-	 
+	  */
+	
+	$select = "SELECT * FROM COMPANY";
+	 $ret = pg_query($db_connection, $sql);
+	   if(!$ret) {
+		  echo pg_last_error($db_connection);
+		  exit;
+	   } 
+	   while($row = pg_fetch_row($ret)) {
+		  echo "ID = ". $row[0] . "\n";
+		  echo "NAME = ". $row[1] ."\n";
+		  echo "ADDRESS = ". $row[2] ."\n";
+		  echo "SALARY =  ".$row[4] ."\n\n";
+	   }
+	   echo "Operation done successfully\n";
+	   pg_close($db_connection);
+
 	
 	
 	
