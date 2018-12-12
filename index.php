e2501e9ca36d343f50b8156c86c81444e566c591
baselinerahul <34484037+baselinerahul@users.noreply.github.com>
2018-12-12 15:04:13 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,12 +3,15 @@
 include('functions.php');
 $data=get_users();
  while($row = pg_fetch_row($data)) {
+	 echo "<pre>";
+	 print_r($row);
+	  echo "<pre>";
 		  echo "ID = ". $row[0] . "\n";
 		  echo "NAME = ". $row[1] ."\n";
 		  echo "ADDRESS = ". $row[2] ."\n";
 		  echo "SALARY =  ".$row[4] ."\n\n";
 	   }
-$db_connection=database_connect();
+/*$db_connection=database_connect();
 	$select = "ALTER TABLE USERS ADD OTP TEXT";
 	 $ret = pg_query($db_connection, $select);
 	   if(!$ret) {
@@ -16,6 +19,6 @@ $db_connection=database_connect();
 		  exit;
 	   }else{ 'edited';
 	   
-	   }
+	   }*/
 ?>
 
