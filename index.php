617e9fc5e4330ca6857276e7858a16a5242c2edb
baselinerahul <34484037+baselinerahul@users.noreply.github.com>
2018-12-12 12:20:14 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
-
-echo "hello test user";
-
-
+    $db_connection = pg_connect("host=ec2-184-72-239-186.compute-1.amazonaws.com
+ dbname=dfkcsq047ej0lu user=fzxqjsjlxieohl password=fbdfcaa97415b4062f460a6f3abda63d4bd1565766cdf382d536c4e66a67cf27");
+     if($db_connection) {
+       echo 'connected';
+    } else {
+        echo 'there has been an error connecting';
+    }
 ?>
