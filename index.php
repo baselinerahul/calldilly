44d118c187fc4ed38a1f0f9c83575bfaecd9f26b
baselinerahul <34484037+baselinerahul@users.noreply.github.com>
2018-12-12 14:36:20 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,14 @@ $data=get_users();
 		  echo "ADDRESS = ". $row[2] ."\n";
 		  echo "SALARY =  ".$row[4] ."\n\n";
 	   }
-
-
+$db_connection=database_connect();
+	$select = "ALTER TABLE USERS ALTER COLUMN ID TYPE SERIAL;";
+	 $ret = pg_query($db_connection, $select);
+	   if(!$ret) {
+		  echo pg_last_error($db_connection);
+		  exit;
+	   }else{ 'edited';
+	   
+	   }
 ?>
 
