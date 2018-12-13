6c71be3fb5ceab227f5afaf5bf43cd02dd798789
baselinerahul <34484037+baselinerahul@users.noreply.github.com>
2018-12-13 11:31:36 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,8 +1,15 @@
 <?php
 
+$login_data = json_decode(file_get_contents("php://input"));
+
+if(!empty($login_data->username) && !empty($login_data->password)) {
+	
+	$log_user = $login_data->username;
+	$log_pass = $login_data->password;
+
 	// Define $username and $password
-	$username= pg_escape_string('abcd');
-	$password= pg_escape_string('2345432456');
+	$username= pg_escape_string($log_user);
+	$password= pg_escape_string($log_pass);
 
 	// Connecting, selecting database
 	include 'config.php';	
@@ -14,14 +21,25 @@
 
 	$rows = pg_num_rows($result);
 	if ($rows == 1) {
-		echo 'Success';
+		echo json_encode(array("message" => "Login Successfully","success"=>"1"));
 	} 
 	else {
 		$error = "Username or Password is invalid";
+		echo json_encode(array("message" => "Username or Password is invalid","success"=>"0"));
 	}
 	// Free resultset
 	pg_free_result($result);
 
 	// Closing connection
 	pg_close($connection);
+		}	
+// tell the user data is incomplete			
+else{
+ 
+    // set response code - 400 bad request
+    http_response_code(400);
+ 
+    // tell the user
+    echo json_encode(array("message" => "Unable to Process Request. Data is incomplete."));
+}	
 ?>
