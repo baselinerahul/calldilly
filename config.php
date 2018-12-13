ab129e8a9d7a5f09e38be60dd5e06e8cd7a4eb7d
baselinerahul <34484037+baselinerahul@users.noreply.github.com>
2018-12-13 08:27:30 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+$connection = pg_connect("host=ec2-184-72-239-186.compute-1.amazonaws.com dbname=dfkcsq047ej0lu user=fzxqjsjlxieohl password=fbdfcaa97415b4062f460a6f3abda63d4bd1565766cdf382d536c4e66a67cf27")
+    or die('Could not connect: ' . pg_last_error());
+?>
