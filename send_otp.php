a1ccf51a5b769a878984d5a58d1e1f67d2162e57
baselinerahul <34484037+baselinerahul@users.noreply.github.com>
2018-12-17 12:54:00 +0530

    Update send_otp.php

--- send_otp.php
+++ send_otp.php
@@ -1,9 +1,11 @@
 <?php
 include 'config.php';
 $data = json_decode(file_get_contents("php://input"));
-if(!empty($data->phone)) {
-	$to = $data->phone;
-	$userid=$data->id;
+
+	//$to = $data->id;
+	//$to = $data->phone;
+	$userid=1;
+	$to = '+919882756610';
 	$sid='AC877bfda2457808ef1730e26c927e08cd';
 	$token='f172487eed89428a85a80f8799fe122c';
 	$url = "https://api.twilio.com/2010-04-01/Accounts/".$sid."/Messages.json";
@@ -30,7 +32,7 @@ if(!empty($data->phone)) {
 	
 	echo echo json_encode(array("message" => "error in sending otp","success"=>"0"));	
 	
-}
+
 
 function generateNumericOTP($n) { 
     $generator = "1357902468";
