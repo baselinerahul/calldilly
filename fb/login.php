a26a173816347c35dd6bafb1d2d9466ac5474235
baselinerahul <34484037+baselinerahul@users.noreply.github.com>
2018-12-18 13:41:46 +0530

    Create login.php

---
+++ fb/login.php
@@ -0,0 +1,16 @@
+<?
+
+$fb = new Facebook\Facebook([
+  'app_id' => '496175707541599', // Replace {app-id} with your app id
+  'app_secret' => 'c104c2722cb29b09a772ad55ce00d95a',
+  'default_graph_version' => 'v2.2',
+  ]);
+
+$helper = $fb->getRedirectLoginHelper();
+
+$permissions = ['email']; // Optional permissions
+$loginUrl = $helper->getLoginUrl('https://example.com/fb-callback.php', $permissions);
+
+echo '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>';
+
+?>
