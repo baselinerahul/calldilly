724c2f4b5319329f435f5cfaabc5f67f17a4c6bb
baselinerahul <34484037+baselinerahul@users.noreply.github.com>
2018-12-24 12:39:36 +0530

    Update accessToken.php

--- accessToken.php
+++ accessToken.php
@@ -1,30 +1,45 @@
 <?php
-/*
- * Creates an access token with VoiceGrant using your Twilio credentials.
- */
-include('./vendor/autoload.php');
-include('./config.php');
+// Get the PHP helper library from https://twilio.com/docs/libraries/php
+require_once 'vendor/autoload.php'; // Loads the library
 use Twilio\Jwt\AccessToken;
 use Twilio\Jwt\Grants\VoiceGrant;
 
-// Use identity and room from query string if provided
-$identity = isset($_GET["identity"]) ? $_GET["identity"] : NULL;
-if (!isset($identity) || empty($identity)) {
-  $identity = isset($_POST["identity"]) ? $_POST["identity"] : "alice";
-}
+// Required for all Twilio access tokens
+$twilioAccountSid = 'AC877bfda2457808ef1730e26c927e08cd';
+$twilioApiKey = 'SKd06e37ab01f0c855ffa85ab6d0cccbc7';
+$twilioApiSecret = '3XwEC81PIpBtRkvpdTKusgWlSTfkofd4';
+
+
+// Required for Voice grant
+$outgoingApplicationSid = 'AP4b094f2ff0e05347303b34e69e8bcac0';
+// An identifier for your app - can be anything you'd like
+$identity = "john_doe";
 
 // Create access token, which we will serialize and send to the client
-$token = new AccessToken($ACCOUNT_SID, 
-                         $API_KEY, 
-                         $API_KEY_SECRET, 
-                         3600, 
-                         $identity
+$token = new AccessToken(
+    $twilioAccountSid,
+    $twilioApiKey,
+    $twilioApiSecret,
+    3600,
+    $identity
 );
 
-// Grant access to Video
-$grant = new VoiceGrant();
-$grant->setOutgoingApplicationSid($APP_SID);
-$grant->setPushCredentialSid($PUSH_CREDENTIAL_SID);
-$token->addGrant($grant);
+// Create Voice grant
+$voiceGrant = new VoiceGrant();
+$voiceGrant->setOutgoingApplicationSid($outgoingApplicationSid);
 
+// Optional: add to allow incoming calls
+$voiceGrant->setIncomingAllow(true);
+
+// Add grant to token
+$token->addGrant($voiceGrant);
+
+// render token to string
 echo $token->toJWT();
+
+echo "<pre>";
+print_r($token);
+echo "</pre>";
+
+?>
+
