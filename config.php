051545563539c0c79df2916be1fa086f654d8c86
baselinerahul <34484037+baselinerahul@users.noreply.github.com>
2019-01-07 14:03:04 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,11 +1,14 @@
 <?php
 $ACCOUNT_SID = 'AC877bfda2457808ef1730e26c927e08cd';
-$API_KEY = 'SKd06e37ab01f0c855ffa85ab6d0cccbc7';
-$API_KEY_SECRET = '3XwEC81PIpBtRkvpdTKusgWlSTfkofd4';
+$API_KEY = 'SK04a9d7849410958a262310ad12df2964';
+$API_KEY_SECRET = 'kRmbWcPOAsWYe54p07Cj6XtlLYsiIQxq';
 $PUSH_CREDENTIAL_SID = 'CRe1791a6c31e7e7036ff849b3c561b699';
 $APP_SID = 'AP4b094f2ff0e05347303b34e69e8bcac0';
 $connection = pg_connect("host=ec2-184-72-239-186.compute-1.amazonaws.com dbname=dfkcsq047ej0lu user=fzxqjsjlxieohl password=fbdfcaa97415b4062f460a6f3abda63d4bd1565766cdf382d536c4e66a67cf27")
     or die('Could not connect: ' . pg_last_error());
 // PNee9ac65d5ec1fa3e1cae49d12e1311e8
+//SK04a9d7849410958a262310ad12df2964
+//kRmbWcPOAsWYe54p07Cj6XtlLYsiIQxq
+ //   $API_KEY_SECRET = '3XwEC81PIpBtRkvpdTKusgWlSTfkofd4';
 ?>
 
