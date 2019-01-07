96bb1b0f83f62d598c7e2b8dd6a4ae7a6cbd0e1a
baselinerahul <34484037+baselinerahul@users.noreply.github.com>
2019-01-07 09:11:10 +0530

    Update makeCall.php

--- makeCall.php
+++ makeCall.php
@@ -9,32 +9,4 @@
 include('./vendor/autoload.php');
 include('./config.php');
 
-$callerId = 'client:quick_start';
-$to = isset($_POST["to"]) ? $_POST["to"] : "";
-if (!isset($to) || empty($to)) {
-  $to = isset($_GET["to"]) ? $_GET["to"] : "";
-}
-
-/*
- * Use a valid Twilio number by adding to your account via https://www.twilio.com/console/phone-numbers/verified
- */
-$callerNumber = '14388342203';
-
-$response = new Twilio\Twiml();
-if (!isset($to) || empty($to)) {
-  $response->say('Congratulations! You have just made your first call! Good bye.');
-} else if (is_numeric($to)) {
-  $dial = $response->dial(
-    array(
-  	  'callerId' => $callerNumber
-  	));
-  $dial->number($to);
-} else {
-  $dial = $response->dial(
-    array(
-       'callerId' => $callerId
-    ));
-  $dial->client($to);
-}
-
-print $response;
+file_put_contents('test.txt',print_r($_POST,true));
