b708d0d34540b053c546bd558a9bdcb53dba9582
baselinerahul <34484037+baselinerahul@users.noreply.github.com>
2019-01-07 13:42:19 +0530

    Update incoming.php

--- incoming.php
+++ incoming.php
@@ -2,8 +2,8 @@
 /*
  * Creates an endpoint that plays back a greeting.
  */
-include('./vendor/autoload.php');
-include('./config.php');
+include('vendor/autoload.php');
+include('config.php');
 
 $response = new Twilio\Twiml();
 $response->say('Congratulations! You have received your first inbound call! Good bye.');
