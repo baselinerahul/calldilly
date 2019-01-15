573cc2651a8096f5963a900dae013faca7282414
baselinerahul <34484037+baselinerahul@users.noreply.github.com>
2019-01-15 13:09:43 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
-$ACCOUNT_SID = 'AC8ab3c778835e6fca5bc5586277c422ea';
-$API_KEY = 'SK865aec8203fe410a525b0c0e79e4129b';
-$API_KEY_SECRET = 'QyicHIKRlURn3KuIk17oirU3wI7XxW3Q';
-$PUSH_CREDENTIAL_SID = 'CRac04791cfbff409780bdbc7b5deeba30';
-$APP_SID = 'APbef1c183bb8a4d31ad53127da7b480ab';
+$ACCOUNT_SID = 'AC3d1de74dcd2cf7937d48a2f7b3b8ddf5';
+$API_KEY = 'SKdcd7c1ef65c169ea2f4407824c19b523';
+$API_KEY_SECRET = 'o8qWGyka8BnjGtLMUmVlTfgQrU8Z4huz';
+$PUSH_CREDENTIAL_SID = 'CR97b4ff3d12a034e1f5d6e20f2b6dc01b';
+$APP_SID = 'AP3e42d8ee61d44e93b5a365b7f0adc0ba';
 $connection = pg_connect("host=ec2-184-72-239-186.compute-1.amazonaws.com dbname=dfkcsq047ej0lu user=fzxqjsjlxieohl password=fbdfcaa97415b4062f460a6f3abda63d4bd1565766cdf382d536c4e66a67cf27")
     or die('Could not connect: ' . pg_last_error());
 ?>
