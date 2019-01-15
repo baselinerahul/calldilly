8d5a67f23f936ba3c6bb6af55b795e6cbfbee951
baselinerahul <34484037+baselinerahul@users.noreply.github.com>
2019-01-15 08:49:16 +0530

    Update placeCall.php

--- placeCall.php
+++ placeCall.php
@@ -4,7 +4,6 @@
  */
 include('./vendor/autoload.php');
 include('./config.php');
-
 $identity = 'alice';
 $callerNumber = '19786205652';
 $callerId = 'client:quick_start';
@@ -12,13 +11,7 @@ $to = isset($_GET["to"]) ? $_GET["to"] : "";
 if (!isset($to) || empty($to)) {
   $to = isset($POST["to"]) ? $_POST["to"] : "";
 }
-$ACCOUNT_SID = 'AC8ab3c778835e6fca5bc5586277c422ea';
-$API_KEY = 'SK865aec8203fe410a525b0c0e79e4129b';
-$API_KEY_SECRET = 'QyicHIKRlURn3KuIk17oirU3wI7XxW3Q';
-//$PUSH_CREDENTIAL_SID = 'CRe1791a6c31e7e7036ff849b3c561b699';
-
 $client = new Twilio\Rest\Client($API_KEY, $API_KEY_SECRET, $ACCOUNT_SID);
-
 $call = NULL;
 if (!isset($to) || empty($to)) {
   $call = $client->calls->create(
@@ -45,5 +38,4 @@ if (!isset($to) || empty($to)) {
     )
   );
 }
-
 print $call.sid;
