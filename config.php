693bddf008c23ae3e1d9d889ead5ee6ddf02e198
baselinerahul <34484037+baselinerahul@users.noreply.github.com>
2019-01-15 15:56:35 +0530

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 $ACCOUNT_SID = 'AC3d1de74dcd2cf7937d48a2f7b3b8ddf5';
 $API_KEY = 'SK889d08e08efb39528cbb64acbcac16d7';
 $API_KEY_SECRET = '7EVi02Y77YUD3jbqLHZuaddczBFYvHcc';
-$PUSH_CREDENTIAL_SID = 'CR5239ae768818dc26de1f3bc0ac49621c';
+$PUSH_CREDENTIAL_SID = 'CR421c2cb346639257cc36ee7fb6def11d';
 $APP_SID = 'APde30372c6aca4f75b4243876d6a0b9dd';
 $connection = pg_connect("host=ec2-184-72-239-186.compute-1.amazonaws.com dbname=dfkcsq047ej0lu user=fzxqjsjlxieohl password=fbdfcaa97415b4062f460a6f3abda63d4bd1565766cdf382d536c4e66a67cf27")
     or die('Could not connect: ' . pg_last_error());
