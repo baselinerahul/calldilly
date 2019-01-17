aeb34430aa9a81b0cf8c828ac259fb727d7ad973
baselinerahul <34484037+baselinerahul@users.noreply.github.com>
2019-01-17 14:34:49 +0530

    Update incoming.php

--- incoming.php
+++ incoming.php
@@ -5,5 +5,5 @@
 include('./vendor/autoload.php');
 include('./config.php');
 $response = new Twilio\Twiml();
-$response->say('Congratulations! You have received your first inbound call! Good bye.');
+$response->say('You have received your first inbound call.');
 print $response;
