23394f86723e7f0b8bcaae0731ab12acb87b84f9
baselinerahul <34484037+baselinerahul@users.noreply.github.com>
2019-01-18 15:23:22 +0530

    Update sms.php

--- sms.php
+++ sms.php
@@ -1,12 +1,15 @@
 <?php
+$data = json_decode(file_get_contents("php://input"));
+if(!empty($data->id)){
+	
+	$to = $data->to; 
+	$body = $data->body; 
+
 $sid='AC877bfda2457808ef1730e26c927e08cd';
 	$token='f172487eed89428a85a80f8799fe122c';
 	$url = "https://api.twilio.com/2010-04-01/Accounts/".$sid."/Messages.json";
 	$from = "+14388342203";
-	$to = "+918077981043"; 
-
 	
-	$body = "Tomorrow's forecast in Financial District, San Francisco is Clear.";
 	$data = array (
 			'From' => $from,
 			'To' => $to,
@@ -21,5 +24,8 @@ $sid='AC877bfda2457808ef1730e26c927e08cd';
 	curl_setopt($x, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
 	curl_setopt($x, CURLOPT_POSTFIELDS, $post);
 	var_dump($post);
-	$y = json_decode(curl_exec($x));
+	echo $y = json_decode(curl_exec($x));
+}else{
+ echo $y = json_decode('error');
+}
 ?>
