333e9835aaffc1184475d51704c4bcac697863f6
baselinerahul <34484037+baselinerahul@users.noreply.github.com>
2019-01-18 14:19:08 +0530

    Update sms.php

--- sms.php
+++ sms.php
@@ -7,9 +7,9 @@ $sid = "AC877bfda2457808ef1730e26c927e08cd";
 $token = "f172487eed89428a85a80f8799fe122c";
 $client = new Client($sid, $token);
 $client->messages->create(
-  "+16518675309",
+  "+14388342203",
   array(
-    'from' => "+14388342203",
+    'from' => "+918077981043",
     'body' => "Tomorrow's forecast in Financial District, San Francisco is Clear.",
     'mediaUrl' => "https://climacons.herokuapp.com/clear.png",
   )
