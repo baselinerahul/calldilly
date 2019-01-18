bee85e2095c364278e8af63dc528fea2c183e29e
baselinerahul <34484037+baselinerahul@users.noreply.github.com>
2019-01-18 11:46:23 +0530

    Removed Blank Spaces

--- config.php
+++ config.php
@@ -7,4 +7,3 @@ $APP_SID = 'AP4b094f2ff0e05347303b34e69e8bcac0';
 $connection = pg_connect("host=ec2-184-72-239-186.compute-1.amazonaws.com dbname=dfkcsq047ej0lu user=fzxqjsjlxieohl password=fbdfcaa97415b4062f460a6f3abda63d4bd1565766cdf382d536c4e66a67cf27")
     or die('Could not connect: ' . pg_last_error());
 ?>
-
