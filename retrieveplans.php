7bd2e13f9ed0daa4fb6d0e90f6cae7f40e279098
baselinerahul <34484037+baselinerahul@users.noreply.github.com>
2019-01-21 11:36:00 +0530

    Update retrieveplans.php

--- retrieveplans.php
+++ retrieveplans.php
@@ -1,6 +1,6 @@
 <?php
-define('STRIPE_SECRET_KEY','[YOUR_SECRET_API_KEY]');
-define('STRIPE_PUBLIC_KEY','[YOUR_PUBLISHEABLE_API_KEY]');
+define('STRIPE_SECRET_KEY','[sk_test_zUpltxJ97dKU7CEVARagjE8U]');
+define('STRIPE_PUBLIC_KEY','[pk_test_cJhtOjA46tUKvSQIOPxsybmO]');
 header('Content-Type: application/json');
 $results = array();
 require 'vendor/autoload.php';
