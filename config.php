5d5b19d9a2420d65ff7f4b2a937f555bf6a9c08c
baselinerahul <34484037+baselinerahul@users.noreply.github.com>
2019-01-21 08:34:48 +0530

    Update config.php

--- config.php
+++ config.php
@@ -5,8 +5,8 @@
 //$PUSH_CREDENTIAL_SID = 'CRc62674116d636c79a4e14f5858c0158e';
 //$APP_SID = 'AP4b094f2ff0e05347303b34e69e8bcac0';CRe1791a6c31e7e7036ff849b3c561b699
 $ACCOUNT_SID = 'AC3d1de74dcd2cf7937d48a2f7b3b8ddf5';  
-$API_KEY = 'SK79da8f31d26412438a3fd50b94a8236b';
-$API_KEY_SECRET = 'cceNlnHZHj6HEbxK2PHxqtM6lhA8ms7x';
+$API_KEY = 'SKc58b0e8881f6afde4e3b7de5a56dcbc0';
+$API_KEY_SECRET = 'EyrOzs97Swlh47yibGTcuqCXCWKPEdwi';
 $PUSH_CREDENTIAL_SID = 'CR0fa3170aa31e3c9a279f42dc49ffd1e4'; 
 $APP_SID = 'AP4b094f2ff0e05347303b34e69e8bcac0';
 $connection = pg_connect("host=ec2-184-72-239-186.compute-1.amazonaws.com dbname=dfkcsq047ej0lu user=fzxqjsjlxieohl password=fbdfcaa97415b4062f460a6f3abda63d4bd1565766cdf382d536c4e66a67cf27")
