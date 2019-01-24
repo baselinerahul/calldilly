3b1cb64931b44c6615089d96fb3a8aeb514350c7
baselinerahul <34484037+baselinerahul@users.noreply.github.com>
2019-01-24 13:51:23 +0530

    Add calllog file

--- callLog.php
+++ callLog.php
@@ -1,1 +1,22 @@
-
+<?php
+$data = json_decode(file_get_contents("php://input"));
+//print_r($data);
+if(!empty($data)){
+	
+	$To = $data->to; 
+	$From = $data->from; 
+  
+  $sid='AC3d1de74dcd2cf7937d48a2f7b3b8ddf5';
+	$token='656b0945465986c010729b6a189890fe';
+	$url = "https://api.twilio.com/2010-04-01/Accounts/".$sid."/Calls.json?To=".$To."&From=".$From;
+	
+	$x = curl_init($url );
+	curl_setopt($x, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($x, CURLOPT_USERPWD, "$sid:$token");
+	curl_setopt($x, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+	$y = curl_exec($x);
+	echo $y; 
+}else{
+ echo $y = json_decode('error');
+}
+?>
